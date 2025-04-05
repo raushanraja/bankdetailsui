d5fb1eeaa9c1760a16546479c97935d548f1c8a2
raushanraja <vraushanraja@gmail.com>
2025-04-05 11:28:25 +0530

    feat: add base URL for GitHub Pages deployment
    - Added base URL configuration to vite.config.ts
    - Ensures correct serving of the application from the specified base path
    - Supports deployment to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import tailwindcss from '@tailwindcss/vite'
 import solid from 'vite-plugin-solid'
 
 export default defineConfig({
+    base: '/bankdetailsui/',
     plugins: [tailwindcss(), solid()],
 })
